f1576dfb281040ce7fa7d7a22cb1b710d8ce5be5
JoaquinMend <joaquinmc0902@gmail.com>
2022-03-18 23:32:30 -0500

    Primer avance

---
+++ Example.py
@@ -0,0 +1,10 @@
+print("""===========================
+   Seleccione una opción   
+===========================
+ 
+1. Mostrar Listado de Archivo Texto
+2. Registrar Usuario
+3. Eliminar Registro
+4. Inicio
+ """)
+e = int(input("Opción: "))
